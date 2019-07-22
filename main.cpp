b1744b14dec58f26bda5be930bfe7cfff90e3920
Aidan Prehatny <aprehatny@yahoo.com>
2019-07-21 18:14:24 -0700

    test vector class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+#include <iostream>
 #include "vec.hpp"
 
 int main() {
     Vec<int> vec;
+    int foo[] = {1,2,3,4,5};
+    for (int n : foo) vec.push_back(n);
+    for (Vec<int>::size_type i = 0; i < vec.size(); ++i) {
+        std::cout << vec[i] << std::endl;
+    }
     return 0;
 }
